963353e46f8efce427a0c2cd8d550b050c85e3ec
SohalSawardekar <sohalsawardekar11@gmail.com>
2025-04-20 22:48:35 +0530

    bug fix

--- src/components/supabase-provider.jsx
+++ src/components/supabase-provider.jsx
@@ -1,5 +1,8 @@
+"use client";
+
 import { SessionContextProvider } from "@supabase/auth-helpers-react";
 import { supabase } from "@/lib/supabase";
+import React from "react";
 
 export function SupabaseProvider({ children }) {
   return (
